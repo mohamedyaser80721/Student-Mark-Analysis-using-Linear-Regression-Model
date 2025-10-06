7f8706823acc10821f15759fd2fada34c493ea38
mohamedyaser80721 <mohamedyaser80721@gmail.com>
2025-10-06 10:43:46 +0530

    MLmodel.py

--- MLmodel.py
+++ MLmodel.py
@@ -13,17 +13,24 @@ encoder = le.fit_transform(df["Extracurricular Activities"])
 df["Extracurricular Activities"] = encoder
 
 
-x = df[["Hours Studied", "Previous Scores", 
-        "Extracurricular Activities", 
-        "Sleep Hours", 
-        "Sample Question Papers Practiced"]]
+x = df[["Hours Studied", "Previous Scores", "Extracurricular Activities", "Sleep Hours", "Sample Question Papers Practiced"]]
 y = df["Performance Index"] 
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 model = LinearRegression()
 model.fit(x_train.values,y_train.values)
+print(df)
 
-prediction = model.predict([[8,89,1,7,6]])
-print(prediction)
+a = int(input(" Hours Studied (1-9):"))
+b = int(input("Previous Scores(1-100)"))
+c = int(input("Extracurricular Activities Yes (1) or No (0) :"))
+d = int(input("Sleep Hours (1-8):"))
+e = int(input("Sample Question Papers Practiced (1-5): "))
+
+
+
+prediction = model.predict([[a,b,c,d,e]])
+print("prediction for new data",prediction)
+
 joblib.dump(model,"student_prediction")
